184e0fc97f7fb40faed40a44292dd39bee300d49
Joshua Storey & Brandon Madsen <student@epicodus.com>
2014-08-04 10:51:51 -0700

    create tests to determine triangle types, scalene, isoscoles, equilateral, and if it is not a triangle.Tests pass.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,5 +4,29 @@ class Triangle
     @side2 = side2
     @side3 = side3
   end
+
+  def equilateral
+    if @side1 == @side2 && @side2 == @side3
+      "equilateral"
+    end
+  end
+
+  def isoscoles
+    if @side1 == @side2 || @side2 == @side3 || @side3 == side1
+      "isoscoles"
+    end
+  end
+
+  def scalene
+    if @side1 != @side2 && @side2 != @side3 && @side3 != @side1
+      "scalene"
+    end
+  end
+
+  def not_a_triangle
+    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+      "not a triangle"
+    end
+  end
 end
 
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -2,9 +2,29 @@ require'rspec'
 require'triangle'
 
 describe 'Triangle' do
-  it('should initialize equilateral triangle and take 3 sides as arguments') do
+  it('should initialize a triangle and take 3 sides as arguments') do
     test_triangle = Triangle.new(2, 2, 2)
     expect(test_triangle).to be_an_instance_of Triangle
   end
+  it('should determine what a equilateral triangle is') do
+    test_triangle = Triangle.new(4, 4, 4)
+    expect(test_triangle.equilateral).to eq ("equilateral")
+  end
+
+  it('should determine what an isoscoles triangle is') do
+    test_triangle = Triangle.new(1, 1, 2)
+    expect(test_triangle.isoscoles).to eq ('isoscoles')
+  end
+
+  it('should determine what a scalene triangle is') do
+    test_triangle = Triangle.new(2, 4, 3)
+    expect(test_triangle.scalene).to eq ('scalene')
+  end
+
+  it('should determine if it is not a triangle') do
+    test_triangle = Triangle.new(1, 2, 30)
+    expect(test_triangle.not_a_triangle).to eq ('not a triangle')
+  end
 end
+
 
