7a59a12b61e48aa22bf56d9f4e54d7b2191f59e9
Joshua Storey & Brandon Madsen <student@epicodus.com>
2014-08-04 11:08:17 -0700

    refactor code, working tests

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,28 +5,15 @@ class Triangle
     @side3 = side3
   end
 
-  def equilateral
-    if @side1 == @side2 && @side2 == @side3
+  def style
+    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
+      "not a triangle"
+    elsif @side1 == @side2 && @side2 == @side3
       "equilateral"
-    end
-  end
-
-  def isoscoles
-    if @side1 == @side2 || @side2 == @side3 || @side3 == side1
+    elsif @side1 == @side2 || @side2 == @side3 || @side3 == side1
       "isoscoles"
-    end
-  end
-
-  def scalene
-    if @side1 != @side2 && @side2 != @side3 && @side3 != @side1
+    else @side1 != @side2 && @side2 != @side3 && @side3 != @side1
       "scalene"
     end
   end
-
-  def not_a_triangle
-    if @side1 + @side2 < @side3 || @side2 + @side3 < @side1 || @side1 + @side3 < @side2
-      "not a triangle"
-    end
-  end
 end
-
--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -6,25 +6,10 @@ describe 'Triangle' do
     test_triangle = Triangle.new(2, 2, 2)
     expect(test_triangle).to be_an_instance_of Triangle
   end
-  it('should determine what a equilateral triangle is') do
-    test_triangle = Triangle.new(4, 4, 4)
-    expect(test_triangle.equilateral).to eq ("equilateral")
-  end
 
-  it('should determine what an isoscoles triangle is') do
-    test_triangle = Triangle.new(1, 1, 2)
-    expect(test_triangle.isoscoles).to eq ('isoscoles')
-  end
-
-  it('should determine what a scalene triangle is') do
-    test_triangle = Triangle.new(2, 4, 3)
-    expect(test_triangle.scalene).to eq ('scalene')
-  end
-
-  it('should determine if it is not a triangle') do
-    test_triangle = Triangle.new(1, 2, 30)
-    expect(test_triangle.not_a_triangle).to eq ('not a triangle')
+  it("should return the type of triangle") do
+    test_triangle = Triangle.new(2, 2, 2)
+    expect(test_triangle.style).to eq ('equilateral')
   end
 end
-
 
